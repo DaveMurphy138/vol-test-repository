fe3aa93f8508912f5c1c5a718c4e9ca7efd2243d
student <student@safebear.co.uk>
2019-10-03 12:26:06 +0100

    Continuing to create environment for VOL testing

---
+++ e2e/protractor.conf.ts
@@ -0,0 +1,41 @@
+import { browser, Config } from "protractor";
+
+export const config: Config = {
+
+    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+
+    baseUrl: 'https://us.val.mamvol.uk/r/1.8.4/web/authenticate',
+
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+
+    specs: [
+        "../e2e/features/*.feature",
+    ],
+    
+    onPrepare: () => {
+        browser.manage().window().maximize();
+    },
+
+    cucumberOpts: {
+        //Typescript compiler
+        compiler: "ts:ts-node/register",
+        //Report format
+        format: "json:.tmp/results.json",
+        profile: false,
+        //The locations of our step definitions and hooks
+        require: ["./step_definitions/*.steps.js", "./support/hooks.js"],
+        //Tags stating which scenarios we will un
+        tags: false,
+        "no-source": true,
+
+
+        plugins:[{
+            options:{
+                automaticallyGenerateReport: true,
+                removeExistingJsonReportFile: true,
+            },
+            package: 'protractor-multiple-cucumber-html-reporter-plugin',
+        }],
+    }
+}
---
+++ e2e/support/hooks.ts
@@ -0,0 +1,9 @@
+import { browser } from "protractor";
+
+import { After, Before, Status } from "cucumber";
+
+Before({timeout: 100 * 1000}, async function() {
+
+    //Opens the website to the default URL in the 'protractot.config.ts' file
+    await browser.get("");
+}
